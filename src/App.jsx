e05b2f0647ec9c9c092a1938bbf6f9cfe650550d
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-16 22:09:34 +0200

    Add the main styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import './App.css'
 
+import pokemonPic from "./assets/pokemon.png"
+import {useEffect, useState} from "react";
+
 function App() {
+  const [loading,setLoading] = useState(false);
+
+  useEffect(() => {
+    try {
+      setLoading(true)
+
+
+
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setLoading(false)
+    }
+
+  }, []);
 
   return (
     <>
-      <h1>Gotta catch em all!</h1>
+      <main>
+        <header>
+          <div className="img-container">
+            <img src={pokemonPic}/>
+          </div>
+          <div className="button-container">
+            <button>Vorige</button>
+            <button>Volgende</button>
+          </div>
+        </header>
+        <section className="pokemon-container">
+        </section>
+      </main>
     </>
   )
 }
